30a5cad08f2dd676b8bc4d4cd95d9c6dc4d6185d
Sarah <sarahejohnson89@gmail.com>
2021-09-23 15:59:18 -0700

    put request function into fetchBreedDescription and refactored code to returns instead of console.logs... added fetchBreedDescription  and process.argv to index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,23 @@
 const request = require("request");
-const argv = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${argv}`;
 
-request(url, (error, response, body) => {
-  if (error) {
-    return console.log("ERROR", error);
-  }
 
-  const data = JSON.parse(body)[0];
+const fetchBreedDescription = function(breedName, callback) {
 
-  if (!data) {
-    return console.log(`There are no cats by the name of ${argv}`)
-  }
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
 
-  console.log(data.description);
-});
+  request(url, (error, response, body) => {
+    if (error) {
+      return "ERROR", error;
+    }
+
+    const data = JSON.parse(body)[0];
+
+    if (!data) {
+      return "There are no cats by that name";
+    }
+
+    console.log(data.description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
